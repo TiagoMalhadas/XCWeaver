dd84248ea47a52501b9a48b64722d658d0871209
TiagoMalhadas <tiago.malhadas@gmail.com>
2024-03-10 16:07:34 +0000

    init ctx propagation on multi deployer

--- internal/net/call/call.go
+++ internal/net/call/call.go
@@ -1135,8 +1135,9 @@ func (c *serverConnection) runHandler(hmap *HandlerMap, id uint64, msg []byte) {
 	lineageLen := int(binary.LittleEndian.Uint64(msg[49:]))
 	fmt.Println("len of lineage", lineageLen)
 
-	/*lineageBytes := make([]byte, lineageLen)
-	copy(lineageBytes[:], msg[msgHeaderSize:])
+	lineageBytes := make([]byte, lineageLen)
+	fmt.Println(lineageBytes)
+	/*copy(lineageBytes[:], msg[msgHeaderSize:])
 	var lineage []antipode.WriteIdentifier
 	er := json.Unmarshal(lineageBytes, &lineage)
 	if er != nil {
