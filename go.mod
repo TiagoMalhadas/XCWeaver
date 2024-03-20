9c320623bb672c1cd5e577c1db5e46dd1e406d71
TiagoMalhadas <tiago.malhadas@gmail.com>
2024-03-20 18:51:31 +0000

    start mongoDB antipode APIs implementation

--- go.mod
+++ go.mod
@@ -44,6 +44,12 @@ require (
 require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 )
 
 require (
@@ -99,6 +105,7 @@ require (
 	github.com/tklauser/go-sysconf v0.3.11 // indirect
 	github.com/tklauser/numcpus v0.6.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
+	go.mongodb.org/mongo-driver v1.14.0
 	go.opentelemetry.io/otel/metric v1.19.0 // indirect
 	golang.org/x/mod v0.13.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
---
+++ internal/antipode/mongoDB-antipode.go
@@ -0,0 +1,108 @@
+package antipode
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MongoDB struct {
+	clientOptions *options.ClientOptions
+	database      string
+	collection    string
+}
+
+type MongoDBObject struct {
+	key   string
+	value AntiObj
+}
+
+func CreateMongoDB(host string, port string, database string, collection string) MongoDB {
+	return MongoDB{options.Client().ApplyURI("mongodb://" + host + ":" + port),
+		database,
+		collection,
+	}
+}
+
+// devo verificar primeiro se já existe essa key?
+func (m MongoDB) write(ctx context.Context, key string, obj AntiObj) error {
+
+	client, err := mongo.Connect(ctx, m.clientOptions)
+	if err != nil {
+		return err
+	}
+
+	mongoObj := MongoDBObject{key, obj}
+
+	_, err = client.Database(m.database).Collection(m.collection).InsertOne(ctx, mongoObj)
+
+	return err
+}
+
+// posso assumir que não há mais do que um objeto com a mesma key?
+func (m MongoDB) read(ctx context.Context, key string) (AntiObj, error) {
+
+	client, err := mongo.Connect(ctx, m.clientOptions)
+	if err != nil {
+		return AntiObj{}, err
+	}
+
+	filter := bson.D{{"key", key}}
+
+	cursor, err := client.Database(m.database).Collection(m.collection).Find(ctx, filter)
+	if err != nil {
+		return AntiObj{}, err
+	}
+
+	var results []MongoDBObject
+	if err = cursor.All(ctx, &results); err != nil {
+		return AntiObj{}, err
+	}
+
+	if len(results) != 1 {
+		return AntiObj{}, fmt.Errorf("The key %s does not exist.", key)
+	}
+
+	return results[0].value, err
+}
+
+func (m MongoDB) barrier(ctx context.Context, lineage []WriteIdentifier, datastoreID string) error {
+
+	client, err := mongo.Connect(ctx, m.clientOptions)
+	if err != nil {
+		return err
+	}
+
+	for _, writeIdentifier := range lineage {
+		if writeIdentifier.Dtstid == datastoreID {
+			for {
+				filter := bson.D{{"key", writeIdentifier.Key}}
+
+				cursor, err := client.Database(m.database).Collection(m.collection).Find(ctx, filter)
+				if err != nil {
+					return err
+				}
+
+				var results []MongoDBObject
+				if err = cursor.All(ctx, &results); err != nil {
+					return err
+				}
+
+				if len(results) == 0 {
+					continue
+				} else {
+					if results[0].value.Version == writeIdentifier.Version { //the version replication process is already completed
+						break
+					} else { //the version replication process is not yet completed
+						continue
+					}
+				}
+			}
+		}
+	}
+
+	return nil
+}
